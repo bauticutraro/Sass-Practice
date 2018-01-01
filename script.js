60e1dc81b591bd7f6220ec62173eea11f91461d9
Bautista Cutraro <bautistacutraro@gmail.com>
2018-01-01 20:57:05 -0300

    finalizacion de la seccion mis cursos

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const nav = document.querySelector('#main-nav');
 const backdrop = document.querySelector('#modalBackdrop');
+const modalContent = document.querySelector('#modalContent');
+const modal = document.querySelector('#modal');
+const close = document.querySelector('#close');
+const cancel = document.querySelector('#cancel');
+
 let toggleNavClass = () => {
   nav.classList.toggle('show')
   // Add background Color
@@ -7,3 +12,22 @@ let toggleNavClass = () => {
 };
 // Click Handler
 Array.from(document.querySelectorAll('.navToggle')).map(el => el.addEventListener('click', toggleNavClass))
+
+
+close.addEventListener('click', () => {
+  modalContent.classList.add('show2');
+  modal.classList.add('show3');
+  nav.classList.remove('show');
+  backdrop.classList.remove('modalBackdrop')
+
+});
+
+cancel.addEventListener('click', () => {
+  modal.classList.add('show4');
+  setTimeout(() => {
+    modalContent.classList.remove('show2');
+    modal.classList.remove('show3');
+    modal.classList.remove('show4');
+  }, 300);
+
+})
